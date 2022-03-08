7d80278104286a18b33c51c3f3e2e1c2c662eba4
bmahmoudyan <benyaminmahmoudyan@gmail.com>
2022-03-08 16:30:49 +0330

    feature get api

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,32 @@
 import { DefaultLayout } from "../../components/layouts/defaultLayout";
+import { api } from "../../utils/api";
+import { Fragment, useState, useEffect } from "react";
+import {} from "react/cjs/react.development";
 
 export function Home() {
-    return(
-        <DefaultLayout>
-            this is home page!
-        </DefaultLayout>
-    );
-};
+  const [assets, setAssets] = useState([]);
+  const [offset, setOffset] = useState(0);
+  useEffect(() => {
+    async function getApi() {
+      const response = await api.get("assets/", { limit: 10, offset: offset });
+      setAssets(response.data.data);
+    }
+    getApi();
+  }, []);
+  function renderFarm() {
+    return assets.map((item) => {
+      return (
+        <Fragment>
+          <li key={item.id}>
+            {item.name}
+          </li><br />
+        </Fragment>
+      );
+    });
+  }
+  return (
+    <DefaultLayout>
+      <ul>{renderFarm()}</ul>
+    </DefaultLayout>
+  );
+}
---
+++ src/utils/api/index.js
@@ -0,0 +1,5 @@
+import { create } from "apisauce";
+
+export const api = create({
+    baseURL: "https://api.coincap.io/v2/",
+});
