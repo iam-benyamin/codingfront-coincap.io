7d65a6290316565b4dd4a2c165dbd6e401b030c5
bmahmoudyan <benyaminmahmoudyan@gmail.com>
2022-04-01 23:28:02 +0430

    get start coin detail page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Swap } from "./pages/swap";
+import { Detail } from './pages/detail';
 
 export function Pathes() {
   return (
@@ -8,6 +9,7 @@ export function Pathes() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/swap"  element={<Swap />} />
+        <Route path="/detail"  element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/detail/index.js
@@ -0,0 +1,11 @@
+import { DefaultLayout } from "../../components/layouts/defaultLayout";
+
+export function Detail() {
+    return(
+        <DefaultLayout>
+            <h2>
+                this is coin datail chart page.
+            </h2>
+        </DefaultLayout>
+    )
+}
