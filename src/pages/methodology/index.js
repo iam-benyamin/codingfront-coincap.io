e34a40021ee32cec9431728ee94393f4af31aa6f
bmahmoudyan <benyaminmahmoudyan@gmail.com>
2022-04-03 20:52:51 +0430

    create exchanges and methodology page

---
+++ src/pages/methodology/index.js
@@ -0,0 +1,9 @@
+import { DefaultLayout } from "../../components/layouts/defaultLayout";
+
+export function Methodology() {
+    return(
+        <DefaultLayout>
+            this is Methodology page
+        </DefaultLayout>
+    );
+};
