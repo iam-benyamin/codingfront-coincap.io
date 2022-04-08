696c678da69566af10448ec48ce4ecb62205b5c0
bmahmoudyan <benyaminmahmoudyan@gmail.com>
2022-04-08 19:07:56 +0430

    getstart coin detail page

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,11 +1,42 @@
 import { DefaultLayout } from "../../components/layouts/defaultLayout";
+import { useParams } from "react-router-dom";
+import { Banner, Chart } from './style';
+import { api } from "../../utils/api";
+import { useEffect, useState } from 'react';
 
 export function Detail() {
+    // TODO render farm 
+    // TODO style date 
+    // TODO chart section 
+    // TODO swap section
+    // TODO table section
+    const coin = useParams();
+    const [isLoading, setIsLoading] = useState(false);
+    const [assets, setAssets] = useState([]);
+    useEffect(() => {
+        async function getApi() {
+            setIsLoading(true);
+            const response = await api.get(`assets/${coin.id}`)
+            setAssets(response.data.data);
+            setIsLoading(false);
+        }
+        getApi();
+    }, []);
     return(
         <DefaultLayout>
-            <h2>
-                this is coin datail chart page.
-            </h2>
+            <div style={{display: isLoading ? "none" : "block"}}>
+                <Banner>
+                    <div className="container">
+                        <div className="banner-content">
+                            <p>{assets.rank}</p>
+                            <p>{assets.name}</p>
+                            <p>{assets.symbol}</p>
+                            <p>{assets.priceUsd}</p>
+                        </div>
+                    </div>
+                </Banner>
+                <Chart>stazi</Chart>
+            </div>
         </DefaultLayout>
     )
 }
---
+++ src/pages/detail/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Banner = styled.div`
+    background: linear-gradient(to right, rgb(63, 81, 181), rgb(100, 181, 246));
+    height: 100px;
+`
+export const Chart = styled.div`
+`
